297320e77ecb3a94c6b3654c4ae0981314957541
alexlogy <alex@alexlogy.io>
2022-11-12 23:34:25 +0800

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // call other seeders
+        $this->call([
+            UserSeeder::class,
+            PaperSeeder::class,
+        ]);
     }
 }
